37fb619c6625007b89b82ac55b074f60a3f23261
anton <a@shbk.me>
2020-08-14 21:00:09 +0300

    Add support of batch PUT requests

--- Metaflow.Orleans/API/PutController.cs
+++ Metaflow.Orleans/API/PutController.cs
@@ -1,4 +1,8 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -15,16 +19,40 @@ namespace Metaflow.Orleans
         }
 
         [HttpPut(DefaultActionConvention.DefaultRoute)]
-        public virtual async Task<IActionResult> Respond(string id, TResource input, CancellationToken cancellationToken)
+        public virtual async Task<IActionResult> Respond(string id, CancellationToken cancellationToken)
         {
             var grain = GetGrain(id);
 
-            Result<TResource> result = await grain.Put(input);
+            using var streamReader = new StreamReader(this.Request.Body, Encoding.UTF8);
+            var body = await streamReader.ReadToEndAsync();
 
-            TGrain state = await grain.Get();
+            if (this.Request.Headers.Keys.Contains("Metaflow-Batch"))
+            {
+                var input = JsonSerializer.Deserialize<List<TResource>>(body);
 
-            return result.OK ? Ok(state) : (IActionResult)BadRequest(result.Reason);
+                var results = new List<Result<TResource>>();
+
+                foreach (var i in input)
+                    results.Add(await grain.Put(i));
+
+                var errors = results.Where(r => !r.OK).ToList();
+
+                TGrain state = await grain.Get();
+
+                return !errors.Any() ? Ok(state) : (IActionResult)BadRequest(new { state, errors });
+            }
+            else
+            {
+                var input = JsonSerializer.Deserialize<TResource>(body);
+
+                Result<TResource> result = await grain.Put(input);
+
+                TGrain state = await grain.Get();
+
+                return result.OK ? Ok(state) : (IActionResult)BadRequest(result.Reason);
+            }
         }
+
     }
 
 }
