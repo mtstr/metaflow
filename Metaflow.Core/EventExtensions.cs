4140359db1c4a1316ddab14dae60cce05e9bf726
anton <a@shbk.me>
2020-09-29 20:17:18 +0300

    Fix extension method for Replaced event

--- Metaflow.Core/EventExtensions.cs
+++ Metaflow.Core/EventExtensions.cs
@@ -2,22 +2,26 @@ using System.Collections.Generic;
 
 namespace Metaflow
 {
-    public static class EventExtensions {
-        public static string Name<T,TInput>(this object @event){
-                if (@event is Created<T>) return "Created";
-                if (@event is Rejected<T,TInput>) return "Rejected";
-                if (@event is Deleted<T>) return "Deleted";
-                if (@event is Ignored<T,TInput>) return "Ignored";
-                if (@event is Replaced<T>) return "Replaced";
-                return "Unknown";
+    public static class EventExtensions
+    {
+        public static string Name<T, TInput>(this object @event)
+        {
+            if (@event is Created<T>) return "Created";
+            if (@event is Rejected<T, TInput>) return "Rejected";
+            if (@event is Deleted<T>) return "Deleted";
+            if (@event is Ignored<T, TInput>) return "Ignored";
+            if (@event is Replaced<T>) return "Replaced";
+            return "Unknown";
         }
 
-        public static IEnumerable<object> Created<T>(this T entity){
-            return new List<object>(){ new Created<T>(entity) };
+        public static IEnumerable<object> Created<T>(this T entity)
+        {
+            return new List<object>() { new Created<T>(entity) };
         }
 
-        public static IEnumerable<object> Replaced<T>(this T after, T before){
-            return new List<object>(){ new Replaced<T>(after, before) };
+        public static IEnumerable<object> Replaced<T>(this T after, T before)
+        {
+            return new List<object>() { new Replaced<T>(before, after) };
         }
     }
 }
