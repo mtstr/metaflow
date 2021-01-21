7bf2f21bd6465fc2e6dde94f90c7ecf891654361
anton <a@shbk.me>
2021-01-21 12:55:32 +0200

    Fix event deserialization

--- Metaflow.Orleans/Grains/EventSerializer.cs
+++ Metaflow.Orleans/Grains/EventSerializer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json;
+using Microsoft.FSharp.Core;
 
 namespace Metaflow.Orleans
 {
@@ -12,6 +13,11 @@ namespace Metaflow.Orleans
         private static Type ResolvePropertyType(PropertyInfo pi)
         {
             var t = pi.PropertyType;
+
+            if (t.IsGenericType && t.GetGenericTypeDefinition()==typeof(FSharpOption<>))
+            {
+                return t.GetGenericArguments().First();
+            }
 
             if (t.IsGenericType && t.IsAssignableTo(typeof(IEnumerable)))
             {
@@ -31,17 +37,17 @@ namespace Metaflow.Orleans
         {
             string[] split = eventType.Split(":");
             (var action, var data) = (split[0], split[1]);
-            
+
             List<Type> ownedTypes = type.GetProperties().Select(ResolvePropertyType).ToList();
-            
+
             var eventDataType = ownedTypes.FirstOrDefault(ot => ot.Name == data);
-            
+
             var targetType = (action, data) switch
             {
                 ("Created", _) when data == type.Name => typeof(Created<>).MakeGenericType(type),
                 ("Upgraded", _) when data == type.Name => typeof(Upgraded<>).MakeGenericType(type),
                 ("Created", _) when eventDataType != null => typeof(Created<>).MakeGenericType(eventDataType),
-                
+
                 ("Replaced", _) when eventDataType != null => typeof(Replaced<>).MakeGenericType(eventDataType),
                 ("Deleted", _) when eventDataType != null => typeof(Deleted<>).MakeGenericType(eventDataType),
                 ("Deleted", _) when data == type.Name => typeof(Created<>).MakeGenericType(type),
